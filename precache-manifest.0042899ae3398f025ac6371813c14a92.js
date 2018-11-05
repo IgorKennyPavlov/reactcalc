2fb7eae28ef96460273cde0dabd850b9d085c113
IgorKennyPavlov <kenny05.92@mail.ru>
2018-11-06 01:04:00 +0400

    Add files via upload

---
+++ precache-manifest.0042899ae3398f025ac6371813c14a92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/calcreact/static/media/calc.1e670d88.ttf"
+  },
+  {
+    "revision": "140450db3ab7d02178fb",
+    "url": "/calcreact/static/js/runtime~main.140450db.js"
+  },
+  {
+    "revision": "4e2ee974528499b06816",
+    "url": "/calcreact/static/js/main.4e2ee974.chunk.js"
+  },
+  {
+    "revision": "bbfd270c8329b1c0bcad",
+    "url": "/calcreact/static/js/1.bbfd270c.chunk.js"
+  },
+  {
+    "revision": "4e2ee974528499b06816",
+    "url": "/calcreact/static/css/main.c302748d.chunk.css"
+  },
+  {
+    "revision": "81fb3e20723e4d4de4cbb2c4ef50fba0",
+    "url": "/calcreact/index.html"
+  }
+];
